01b2e928483c828c6a237008d6e399e569ae17d3
Tom Sand <tsand@usc.edu>
2020-09-16 09:33:48 -0700

    added pickle to mailboxmanager

--- ee250/lab04/pickle_test.py
+++ ee250/lab04/pickle_test.py
@@ -5,15 +5,16 @@ notes = []
 
 # A. If notes.pickle exists, read it in using pickle and assign the content to
 #   the notes variable
-path = './notes.pickle'                               # path to pickle file
-if os.path.isfile(path):                              # checks if file exists
-   notes = pickle.load(open('notes.pickle', "rb"))    # loads file
+path = 'notes.pickle'                                    # path to pickle file
+if os.path.isfile(path):                                 # checks if file exists   
+   notes = pickle.load(open('notes.pickle', "rb"))       # loads file
+   
 # B. Print out notes
 print(notes)
 
 # C. Read in a string from the user using input() and append it to notes
-addition = input("Add to Notes: ") # gets new addition
-notes.append(addition)             # adds it to notes
+addition = input("Add to Notes: ")                       # gets new addition
+notes.append(addition)                                   # adds it to notes
 
 # D. Save notes to notes.pickle
 pickle.dump(notes, open("notes.pickle", "wb"))
