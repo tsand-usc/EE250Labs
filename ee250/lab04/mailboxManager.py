f9dc2a4b7d69352565253d52dcde2df09af7af44
Tom Sand <tsand@usc.edu>
2020-09-16 20:46:47 -0700

    fixed eof error handeling

--- ee250/lab04/mailboxManager.py
+++ ee250/lab04/mailboxManager.py
@@ -21,8 +21,10 @@ class mailboxManager(object):
             with open(MAIL_DB_FILE, 'rb') as f:
                 print('Loading {}'.format(MAIL_DB_FILE))
                 # load the pickle data into self.mailbox
-                self.mailbox = pickle.load(f)     
-
+                self.mailbox = pickle.load(f) 
+                  
+        except EOFError:   
+            pass
         except FileNotFoundError:
             pass
 
